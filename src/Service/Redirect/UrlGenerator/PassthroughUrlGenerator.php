eeb53d8dbc6394855306f4c72431a85bfbd1e41e
Anton Zoffmann <anton_sylvester@msn.com>
2017-12-03 18:02:46 +0100

    added pass-through url generator strategy
    (cherry picked from commit 8e2d6a8)

---
+++ src/Service/Redirect/UrlGenerator/PassthroughUrlGenerator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service\Redirect\UrlGenerator;
+
+
+class PassthroughUrlGenerator implements UrlGenerator
+{
+
+    /**
+     * just pass-through the configured redirect-path
+     * @param string $path
+     * @return string
+     */
+    public function generate(string $path): string
+    {
+        return $path;
+    }
+
+}
