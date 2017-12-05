84f6a82426b71b2f53cbafc0d3de20dc4e588d78
Anton Zoffmann <anton_sylvester@msn.com>
2017-12-05 19:02:31 +0100

    added NullCacheTest

--- tests/Cache/NullCacheTest.php
+++ tests/Cache/NullCacheTest.php
@@ -9,7 +9,74 @@
 namespace Jidoka1902\RedirectingFallbacks\Tests\Cache;
 
 
-class NullCacheTest
+use Jidoka1902\RedirectingFallbacks\Cache\NullCache;
+use Jidoka1902\RedirectingFallbacks\Cache\RedirectResolverCache;
+use PHPUnit\Framework\TestCase;
+
+class NullCacheTest extends TestCase
 {
+
+    /** @var RedirectResolverCache */
+    private $cache;
+
+    protected function setUp()
+    {
+        $this->cache = new NullCache();
+    }
+
+    /**
+     * make sure the null cash never hits
+     * @param $requestPath
+     * @param $testValue
+     * @param $expectedResult
+     * @dataProvider getIsHitTestData
+     */
+    public function testIsHit($requestPath, $testValue, $expectedResult)
+    {
+        $this->cache->set($requestPath, "");
+
+        $actualResult = $this->cache->isHit($testValue);
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    /**
+     * @return array
+     */
+    public function getIsHitTestData(): array
+    {
+        return [
+            "test case 1" => ["/test", "/test", false],
+            "test case 2" => ["/test", "/no-test", false],
+        ];
+    }
+
+    /**
+     * make sure a set value will never be returned from the getter
+     * @param $requestPath
+     * @param $resolvedPath
+     * @param $testValue
+     * @param $expectedResult
+     * @dataProvider getSetTestData
+     */
+    public function testSet($requestPath, $resolvedPath, $testValue, $expectedResult)
+    {
+        $this->cache->set($requestPath, $resolvedPath);
+
+        $actualResult = $this->cache->get($testValue);
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    /**
+     * @return array
+     */
+    public function getSetTestData(): array
+    {
+        return [
+            "test case 1" => ["/request", "/injected", "/request", null],
+            "test case 2" => ["/request", "/not-injected", "/no-request", null],
+        ];
+    }
 
 }
